f7449d0675ac021a3519ae84a6499b0aa46108bb
Oliver Santschi <oliver.santschi@bit.admin.ch>
2025-03-04 17:23:02 +0100

    JEAP-5197: Add DeploymentLog

--- src/jeap_pipeline/__init__.py
+++ src/jeap_pipeline/__init__.py
@@ -5,3 +5,4 @@ from .pact_pacticipants import get_pacticipant_names
 from .pact_operations import do_can_i_deploy_check, record_deployment
 from .pact_configuration import verify_pact_configuration, is_pact_enabled_for_service_and_stage, is_pact_can_i_deploy_check_enabled
 from .automated_staging import get_next_deployment_stage
+from .deployment_log_operations import put_deployment_state
