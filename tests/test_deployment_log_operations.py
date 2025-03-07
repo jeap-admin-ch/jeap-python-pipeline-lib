f02ab8c11c894868ad6a77b056af287bd883441c
Oliver Santschi <oliver.santschi@bit.admin.ch>
2025-03-07 15:40:10 +0100

    JEAP-5197: Add DeploymentLog

--- tests/test_deployment_log_operations.py
+++ tests/test_deployment_log_operations.py
@@ -2,28 +2,28 @@ import json
 import unittest
 from unittest.mock import patch, Mock
 
-from jeap_pipeline import put_deployment_state, put_to_deployment_log_service, get_previous_deployment_on_environment
+from src.jeap_pipeline import put_deployment_state, put_to_deployment_log_service, get_previous_deployment_on_environment
 
 class TestDeploymentLogOperations(unittest.TestCase):
-    @patch('jeap_pipeline.deployment_log_operations.__request_deployment_log_service')
+    @patch('src.jeap_pipeline.deployment_log_operations.__request_deployment_log_service')
     def test_put_deployment_state_success(self, mock_request):
         mock_request.return_value.status_code = 200
         response = put_deployment_state("http://example.com", "123", "DEPLOYED", "user", "pass")
         self.assertEqual(response.status_code, 200)
 
-    @patch('jeap_pipeline.deployment_log_operations.__request_deployment_log_service')
+    @patch('src.jeap_pipeline.deployment_log_operations.__request_deployment_log_service')
     def test_put_deployment_state_with_message(self, mock_request):
         mock_request.return_value.status_code = 200
         response = put_deployment_state("http://example.com", "123", "DEPLOYED", "user", "pass", message="Deployment successful")
         self.assertEqual(response.status_code, 200)
 
-    @patch('jeap_pipeline.deployment_log_operations.__request_deployment_log_service')
+    @patch('src.jeap_pipeline.deployment_log_operations.__request_deployment_log_service')
     def test_put_to_deployment_log_service_success(self, mock_request):
         mock_request.return_value.status_code = 200
         response = put_to_deployment_log_service("http://example.com", "123", {"key": "value"}, "user", "pass")
         self.assertEqual(response.status_code, 200)
 
-    @patch('jeap_pipeline.deployment_log_operations.__request_deployment_log_service')
+    @patch('src.jeap_pipeline.deployment_log_operations.__request_deployment_log_service')
     def test_get_previous_deployment_on_environment_found(self, mock_request):
         mock_response = Mock()
         mock_response.status_code = 200
@@ -33,7 +33,7 @@ class TestDeploymentLogOperations(unittest.TestCase):
         self.assertIsNotNone(response)
         self.assertEqual(response["deployment"], "data")
 
-    @patch('jeap_pipeline.deployment_log_operations.__request_deployment_log_service')
+    @patch('src.jeap_pipeline.deployment_log_operations.__request_deployment_log_service')
     def test_get_previous_deployment_on_environment_not_found(self, mock_request):
         mock_response = Mock()
         mock_response.status_code = 404
