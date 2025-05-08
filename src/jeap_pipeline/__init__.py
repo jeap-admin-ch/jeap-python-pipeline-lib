3d7e6d45142ddc9370f11c8b04845004788861e1
Fabien Cerf <fabien.cerf@bit.admin.ch>
2025-05-08 09:26:48 +0200

    JEAP-5495: add archrepo_operations

--- src/jeap_pipeline/__init__.py
+++ src/jeap_pipeline/__init__.py
@@ -9,5 +9,6 @@ from .deployment_log_operations import put_deployment_state, put_to_deployment_l
     get_previous_deployment_on_environment, put_artifacts_version, create_deployment_json, \
     get_actual_timestamp, generate_deployment_id, get_commit_details, get_tagged_at, create_change_log
 from .deployment_log_model import DeploymentTarget, ComponentVersion, DeploymentUnit, Link, Deployment, ChangeLog
+from .archrepo_operations import post_openapi_spec_to_archrepo_service
 
 
