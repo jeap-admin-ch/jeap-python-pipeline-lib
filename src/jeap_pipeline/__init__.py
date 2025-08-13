6c6222a2491da0cb7c203da9bcef6e528a4ea2aa
Laris Jacobs <laris.jacobs@bit.admin.ch>
2025-08-13 18:43:37 +0200

    JEAP-5733 - Added is_service_undeployed to init.

--- src/jeap_pipeline/__init__.py
+++ src/jeap_pipeline/__init__.py
@@ -1,6 +1,6 @@
 # __init__.py
 from .ecs_deployment_checker import wait_until_new_deployment_has_occurred
-from .ecs_undeployment_checker import wait_until_undeployment_has_finished
+from .ecs_undeployment_checker import wait_until_undeployment_has_finished, is_service_undeployed
 from .github_dispatch_event import send_dispatch_event
 from .pact_pacticipants import get_pacticipant_names
 from .pact_operations import do_can_i_deploy_check, record_deployment, record_undeployment
